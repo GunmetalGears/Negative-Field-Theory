1846b19d5a9230ea271f5f205c329dc58f767873
The Mayor <The Mayor@TheGameMaker>
2017-09-24 17:29:31 -0700

    NullCrystalOre

---
+++ src/main/java/com/gunmetal/negativefieldtheory/blocks/NullCrystalOre.java
@@ -0,0 +1,12 @@
+package com.gunmetal.negativefieldtheory.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class NullCrystalOre extends Block{
+
+	
+	public NullCrystalOre(Material materialIn) {
+		super(materialIn);
+	}
+}
