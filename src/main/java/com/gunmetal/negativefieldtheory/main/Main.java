b449fd9f5aff969e5d3d34a8b3cfef2c4bf3d316
The Mayor <The Mayor@TheGameMaker>
2017-09-24 17:19:16 -0700

    NFT Main Class update

--- src/main/java/com/gunmetal/negativefieldtheory/main/Main.java
+++ src/main/java/com/gunmetal/negativefieldtheory/main/Main.java
@@ -1,6 +1,34 @@
 //The main class of Negative Field Theory
 package com.gunmetal.negativefieldtheory.main;
 
-public class Main {
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = Main.MODID, version = Main.VERSION, name = Main.NAME)
+public class Main {
+	public static final String NAME = "Negative Field Theory";
+	public static final String MODID = "nft";
+	public static final String VERSION = "1.0";
+	
+	@Instance
+	public static Main instance = new Main();
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent e) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+		
+	}
 }
